b6bd814e00654d6d68e091db111d1161f3873ef3
Raul Rohjans <rauljann@hotmail.com>
2025-07-24 12:40:42 +0100

    Added scene with narrator

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    public TextMeshProUGUI subtitleText;
+    public AudioSource narrationAudio;
+
+    [System.Serializable]
+    public class SubtitleLine
+    {
+        public string text;
+        public float delayBefore;
+        public float duration;
+    }
+
+    public SubtitleLine[] lines;
+    public string nextSceneName = "MainGame";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(PlayIntro());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator PlayIntro()
+    {
+        narrationAudio.Play();
+
+        foreach (var line in lines)
+        {
+            subtitleText.text = "";
+            yield return new WaitForSeconds(line.delayBefore);
+            yield return StartCoroutine(FadeIn(line.text, 1f));
+            yield return new WaitForSeconds(line.duration);
+            yield return StartCoroutine(FadeOut(1f));
+        }
+
+        subtitleText.text = "";
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(nextSceneName);
+    }
+    
+    IEnumerator FadeIn(string text, float duration)
+    {
+        subtitleText.text = text;
+        Color color = subtitleText.color;
+        color.a = 0;
+        subtitleText.color = color;
+
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsed / duration);
+            subtitleText.color = color;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut(float duration)
+    {
+        Color color = subtitleText.color;
+
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Clamp01(1f - (elapsed / duration));
+            subtitleText.color = color;
+            yield return null;
+        }
+
+        subtitleText.text = "";
+    }
+}
